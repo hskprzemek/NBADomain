c0ce3ab343051fc7d495015f77d9df6690baf9d3
hskprzemek <hskprzemek@gmail.com>
2016-01-17 22:30:02 +0100

    Init

---
+++ src/main/java/org/hsk/domain/Player.java
@@ -0,0 +1,13 @@
+package org.hsk.domain;
+
+/**
+ * Created by cartman on 2015-12-13.
+ */
+public class Player {
+
+    private String firstName;
+    private String lastName;
+
+    private Team team;
+
+}
---
+++ src/main/java/org/hsk/domain/Team.java
@@ -0,0 +1,29 @@
+package org.hsk.domain;
+
+import java.util.List;
+
+/**
+ * Created by cartman on 2015-12-13.
+ */
+public class Team {
+
+    private String name;
+
+    private List<Player> players;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+}
